e1248458a48467cd2a657ef9243abb765ef9536d
Evgeny Cherkashin <eugene@irnok.net>
2017-03-31 00:06:03 +0800

    Now it compiles and runs.

--- ssdc.sh
+++ ssdc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar out/artifacts/ssdc_jar/ssdc.jar -i examples/T002.xlsx -k examples/T002a.dslr -s 0 -m false -o examples/results/T002a
+# java -jar out/artifacts/ssdc_jar/ssdc.jar -i examples/T002.xlsx -k examples/T002a.dslr -s 0 -m false -o examples/results/T002a
+java -jar target/ssdc-0.1-jar-with-dependencies.jar -i examples/T002.xlsx -k examples/T002a.dslr -s 0 -m false -o examples/results/T002a
